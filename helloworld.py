8fd68a319a65aa0c19ebf28e91ea541247a50d0f
sjy2000309-cloud <sjy2000309@gmail.com>
2025-10-29 21:45:51 +0000

    添加分支

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,10 @@ print("Hello World")
 # 这是我的第二次提交
 print("欢迎学习Git和GitHub！")
 print("我正在学习版本控制！")
+
+# 多语言问候功能 - 在新分支上开发
+print("\n=== 多语言问候 ===")
+print("English: Hello World!")
+print("中文: 你好，世界！")
+print("Español: ¡Hola Mundo!")
+print("日本語: こんにちは世界！")
